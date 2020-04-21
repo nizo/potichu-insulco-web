f32edcf5924f5310166f3cc049131b8be7f53187
nizo <martin.niznansky@gmail.com>
2020-04-21 14:06:34 +0200

    tracking services

--- functions.php
+++ functions.php
@@ -15,11 +15,6 @@ define('GETLEADS_CSS', GETLEADS_THEMEROOT . '/assets/css');
 define('GETLEADS_SCRIPTS', GETLEADS_THEMEROOT . '/assets/js');
 define('GETLEADS_PHPSCRIPTS', GETLEADS_THEMEROOT . '/assets/php');
 
-
-
-
-
-
 /**
  * ----------------------------------------------------------------------------------------
  * 3.0 - Set up the content width value based on the theme's design.
@@ -78,7 +73,7 @@ if (!function_exists('getleads_setup')) {
             add_image_size('getleads_recent_post', 350, 235, array('center', 'center'));
             add_image_size('getleads-blog-thumb', 770, 335, TRUE);
         }
-       
+
 
         /**
          * Register nav menus.
@@ -121,7 +116,7 @@ include_once get_template_directory() .'/inc/init.php';
 
 function insulco_wp_head() {
 	$ogImage = get_the_post_thumbnail_url();
-	
+
 	if (!$ogImage) {
 		$ogImage = home_url('/wp-content/uploads/static/insulco-og-image.png');
 	}
@@ -135,22 +130,22 @@ function insulco_wp_head() {
     echo '<meta property="og:title" content="Website title" />';
     echo '<meta property="og:description" content="Website description." />';
 	*/
-	
+
 }
 add_action( 'wp_head', 'insulco_wp_head',11);
 
 
 function enque_insulco_style() {
-	if( WP_DEBUG === true ) { 
-		$version = microtime(true);		
+	if( WP_DEBUG === true ) {
+		$version = microtime(true);
 	} else {
 		$version = '9';
 	}
 
-	wp_register_style( 'insulco-style', get_stylesheet_directory_uri() . '/style.css', array(), $version);	
+	wp_register_style( 'insulco-style', get_stylesheet_directory_uri() . '/style.css', array(), $version);
 	wp_enqueue_style( 'insulco-style' );
 }
-	
+
 add_action( 'wp_enqueue_scripts', 'enque_insulco_style',11);
 
 
@@ -161,7 +156,7 @@ function insulcoEfficiency_handler( $atts ){
 		'bg' => ''
     ), $atts );
 
-	return '<div class="efficiency-box" style="background-color: ' . $a['bg']. '"><span class="title">Účinnosť až</span><br/><span class="value">Δ Lw ' . $a['lv']. ' dB</span><div class="subtitle">' . $a['subtitle'] . '</div></div>';    
+	return '<div class="efficiency-box" style="background-color: ' . $a['bg']. '"><span class="title">Účinnosť až</span><br/><span class="value">Δ Lw ' . $a['lv']. ' dB</span><div class="subtitle">' . $a['subtitle'] . '</div></div>';
 }
 add_shortcode( 'insulcoEfficiency', 'insulcoEfficiency_handler' );
 
@@ -171,3 +166,62 @@ function fw_ext_shortodes_table_edit_max_cols( $max ) {
 }
 
 add_filter( 'fw_ext_shortcodes_table_max_columns', 'fw_ext_shortodes_table_edit_max_cols'  );
+
+
+function insulco_web_settings_register( $wp_customize ) {
+
+	$wp_customize->add_setting( 'google_tag_manager_head' , array(
+		'type'		=> 'option',
+		'default'	=> '',
+		'transport'	=> 'refresh',
+	));
+
+	$wp_customize->add_setting( 'google_tag_manager_body' , array(
+		'type'		=> 'option',
+		'default'	=> '',
+		'transport'	=> 'refresh',
+	));
+
+	$wp_customize->add_setting( 'pixel_tracking_code' , array(
+		'type'		=> 'option',
+		'default'	=> '',
+		'transport'	=> 'refresh',
+	));
+
+	// Sections
+	$wp_customize->add_section( 'web_settings_section' , array(
+		'title'      => 'Insulco',
+		'priority'   => 1000,
+	) );
+
+	$wp_customize->add_control(
+		'google_tag_manager_head_control',
+		array(
+			'label'    => 'Google Tag Manager Head',
+			'section'  => 'web_settings_section',
+			'settings' => 'google_tag_manager_head',
+			'type'     => 'text'
+		)
+	);
+
+	$wp_customize->add_control(
+		'google_tag_manager_body_control',
+		array(
+			'label'    => 'Google Tag Manager Body',
+			'section'  => 'web_settings_section',
+			'settings' => 'google_tag_manager_body',
+			'type'     => 'text'
+		)
+	);
+
+	$wp_customize->add_control(
+		'pixel_tracking_code',
+		array(
+			'label'    => 'Pixel Tracking Code',
+			'section'  => 'web_settings_section',
+			'settings' => 'pixel_tracking_code',
+			'type'     => 'textarea'
+		)
+	);
+}
+add_action( 'customize_register', 'insulco_web_settings_register' );
--- header.php
+++ header.php
@@ -19,28 +19,11 @@
         <!-- Favicon, Apple Touch Icons -->
         <?php getleads_site_icon(); ?>
 
-        <?php wp_head(); ?>
-
-<!-- Facebook Pixel Code -->
-<script>
-!function(f,b,e,v,n,t,s)
-{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-n.queue=[];t=b.createElement(e);t.async=!0;
-t.src=v;s=b.getElementsByTagName(e)[0];
-s.parentNode.insertBefore(t,s)}(window,document,'script',
-'https://connect.facebook.net/en_US/fbevents.js');
-
- fbq('init', '247286316142817'); 
-fbq('track', 'PageView');
-</script>
-<noscript>
- <img height="1" width="1" 
-src="https://www.facebook.com/tr?id=247286316142817&ev=PageView&noscript=1"/>
-</noscript>
-<!-- End Facebook Pixel Code -->
-
+        <?php
+			wp_head();
+			echo get_option('pixel_tracking_code', '');
+			echo get_option('google_tag_manager_head', '');
+		?>
 <!-- Google Tag Manager -->
 <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-PWWN4JH');</script>
 <!-- End Google Tag Manager -->
@@ -48,14 +31,9 @@ src="https://www.facebook.com/tr?id=247286316142817&ev=PageView&noscript=1"/>
 
     <body <?php body_class(); ?>  data-spy="scroll" data-target="#main-navbar">
 
-		<!-- Google Tag Manager (noscript) -->
-		<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PWWN4JH" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-		<!-- End Google Tag Manager (noscript) -->
-
-        <?php
+		<?php
+		echo get_option('google_tag_manager_body', '');
         getleads_prerloader();
 
         //Navigation
         get_template_part('content', 'nav');
-
-        
